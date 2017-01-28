dc52c6f5970c58b2306f8353d7e691a7e420d42b
Wes Miser <buddwm@gmail.com>
2017-01-27 18:04:34 -0600

    Added US Central Time Zone into application.rb, created time_formats.en.yml under locales for prettier date/time format and updated views to use it

--- config/application.rb
+++ config/application.rb
@@ -12,5 +12,6 @@ module DcimPortal
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.active_job.queue_adapter = :sidekiq
+    config.time_zone = 'Central Time (US & Canada)'
   end
 end
