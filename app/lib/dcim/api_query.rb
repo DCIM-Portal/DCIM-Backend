DCIM-Portal/DCIM-Backend

commit 21e71d1b8135f9c90c4f4133dec76e9ee0e7b302

Split BmcHost system_model into brand & product

app/models/bmc_host.rb
@@ -40,16 +40,19 @@ class BmcHost < ApplicationRecord
     logger.debug "Getting FRU list..."
     frulist = fru_list
     logger.debug "Parsing FRU list..."
-    model, serial = get_model_and_serial_from_fru_list(frulist)
-    logger.debug "Obtained model \"#{model}\" and serial \"#{serial}\""
+    brand = brand_from_fru_list(frulist)
+    product = product_from_fru_list(frulist)
+    serial = serial_from_fru_list(frulist)
+    logger.debug "Obtained brand \"#{brand}\", product \"#{product}\", and serial \"#{serial}\""
     logger.debug "Getting power status..."
     power_status = power_on? ? :on : :off
     logger.debug "Power status is #{power_status}"
     logger.debug "Updating record with obtained information..."
-    self.system_model  = model
-    self.serial        = serial
-    self.power_status  = power_status
-    self.sync_status   = :success
+    self.brand        = brand
+    self.product      = product
+    self.serial       = serial
+    self.power_status = power_status
+    self.sync_status  = :success
     self.save!
     logger.debug "Record updated!"
   rescue RuntimeError => e
@@ -181,17 +184,28 @@ class BmcHost < ApplicationRecord
     end
   end
 
-  def get_model_and_serial_from_fru_list(fru_list)
-    brand = deep_find('board_manufacturer', fru_list) ||
-            deep_find('board_mfg', fru_list)
-    product = deep_find('product_part/model_number', fru_list) ||
-              deep_find('product_name', fru_list) ||
-              deep_find('board_product_name', fru_list)
-    serial = deep_find('product_serial_number', fru_list)
-    raise Dcim::UnsupportedFruError, fru_list if !brand
-    # TODO: Modify this model to store brand and product separately
-    # so that we can isolate brand and product
-    return "#{brand} #{product}", serial
+  def brand_from_fru_list(fru)
+    output =
+    deep_find('board_manufacturer', fru) ||
+    deep_find('board_mfg', fru)
+    raise Dcim::UnsupportedFruError, fru if !output
+    output
+  end
+
+  def product_from_fru_list(fru)
+    output =
+    deep_find('product_part/model_number', fru) ||
+    deep_find('product_name', fru) ||
+    deep_find('board_product_name', fru)
+    raise Dcim::UnsupportedFruError, fru if !output
+    output
+  end
+
+  def serial_from_fru_list(fru)
+    output =
+    deep_find('product_serial_number', fru)
+    raise Dcim::UnsupportedFruError, fru if !output
+    output
   end
 
   def validate_correct_credentials

app/assets/javascripts/bmc_hosts.coffee
@@ -28,7 +28,8 @@ $(document).on 'turbolinks:load', ->
       columns: [
         {data: 'checkbox'}
         {data: 'ip_address'}
-        {data: 'system_model'}
+        {data: 'brand'}
+        {data: 'product'}
         {data: 'serial'}
         {data: 'zone_name'}
         {data: 'power_status'}
@@ -44,13 +45,14 @@ $(document).on 'turbolinks:load', ->
       select: 'multi'
       createdRow: (row, data, dataIndex) ->
         $(row).find('td:eq(1)').attr 'data-title', 'BMC Address:'
-        $(row).find('td:eq(2)').attr 'data-title', 'System Model:'
-        $(row).find('td:eq(3)').attr 'data-title', 'Serial:'
-        $(row).find('td:eq(4)').attr 'data-title', 'Datacenter Zone:'
-        $(row).find('td:eq(5)').attr 'data-title', 'Power Status:'
-        $(row).find('td:eq(6)').attr 'data-title', 'Last Sync Status:'
-        $(row).find('td:eq(7)').attr 'data-title', 'Onboard Status:'
-        $(row).find('td:eq(8)').attr 'data-title', 'Synchronize Date:'
+        $(row).find('td:eq(2)').attr 'data-title', 'Brand:'
+        $(row).find('td:eq(3)').attr 'data-title', 'Product:'
+        $(row).find('td:eq(4)').attr 'data-title', 'Serial:'
+        $(row).find('td:eq(5)').attr 'data-title', 'Datacenter Zone:'
+        $(row).find('td:eq(6)').attr 'data-title', 'Power Status:'
+        $(row).find('td:eq(7)').attr 'data-title', 'Last Sync Status:'
+        $(row).find('td:eq(8)').attr 'data-title', 'Onboard Status:'
+        $(row).find('td:eq(9)').attr 'data-title', 'Synchronize Date:'
       buttons: [
         {
           extend: 'copyHtml5'
@@ -71,15 +73,15 @@ $(document).on 'turbolinks:load', ->
           selectRow: true
           }
         }
-        { targets: 10
+        { targets: 11
         orderable: false
         }
-        { targets: 8
+        { targets: 9
         orderable: false
         visible: false
         searchable: false
         }
-        { targets: 7
+        { targets: 8
         render: (data, type, full) ->
           if !data
             '<div class="blue-grey lighten-1 white-text z-depth-1 sync"><i class="fa fa-minus-circle" aria-hidden="true"></i> Not Onboarded</div>'
@@ -90,7 +92,7 @@ $(document).on 'turbolinks:load', ->
           else
             '<div class="red lighten-2 white-text z-depth-1 sync"><i class="fa fa-exclamation-triangle" aria-hidden="true"></i> ' + I18n.t(data, scope: 'filters.options.onboard_request.status') + ': ' + I18n.t(full.onboard_request_step, scope: 'filters.options.onboard_request.step') + '</div>' 
         }
-        { targets: 5
+        { targets: 6
         render: (data, type, full, meta) ->
           if data == "on"
             '<div class="power_status green lighten-2 z-depth-1"><i class="fa fa-power-off"></i> On</div>'
@@ -100,7 +102,7 @@ $(document).on 'turbolinks:load', ->
             '<div class="black-text">N/A</div>'
         width: 50
         }
-        { targets: 6
+        { targets: 7
         render: (data, type, full) ->
           if !data
             '<div class="blue-grey darken-2 white-text z-depth-1 sync"><i class="fa fa-hourglass-start" aria-hidden="true"></i> Queued</div>'
@@ -115,7 +117,7 @@ $(document).on 'turbolinks:load', ->
         width: 200
         }
         { targets: 2
-        orderable: false
+        orderable: true
         render: (data, type, full, meta) ->
           if /(HP)/.test(data)
             '<div class="model_wrapper"><div class="img-box"><img src="/images/hpe.png" height=25 width=57 /></div><div class="model_cell">' + data + '</div></div>'
@@ -131,10 +133,10 @@ $(document).on 'turbolinks:load', ->
             '<div class="model_cell">N/A</div>'
           else
             '<div class="model_cell">' + data + '</div>'
-        width: 325
+        width: 175
         }
-        { targets: 3
-        orderable: false
+        { targets: 4
+        orderable: true
         render: (data, type, full, meta) ->
           if data
             '<div class="serial">' + data + '</div>'

app/assets/javascripts/bmc_scan_request.coffee
@@ -120,7 +120,8 @@ $(document).on 'turbolinks:load', ->
       columns: [
         {data: 'checkbox'}
         {data: 'ip_address'}
-        {data: 'system_model'}
+        {data: 'brand'}
+        {data: 'product'}
         {data: 'serial'}
         {data: 'power_status'}
         {data: 'sync_status'}
@@ -135,12 +136,13 @@ $(document).on 'turbolinks:load', ->
       select: 'multi'
       createdRow: (row, data, dataIndex) ->
         $(row).find('td:eq(1)').attr 'data-title', 'BMC Address:'
-        $(row).find('td:eq(2)').attr 'data-title', 'System Model:'
-        $(row).find('td:eq(3)').attr 'data-title', 'Serial:'
-        $(row).find('td:eq(4)').attr 'data-title', 'Power Status:'
-        $(row).find('td:eq(5)').attr 'data-title', 'Last Sync Status:'
-        $(row).find('td:eq(6)').attr 'data-title', 'Onboard Status:'
-        $(row).find('td:eq(7)').attr 'data-title', 'Synchronize Date:'
+        $(row).find('td:eq(2)').attr 'data-title', 'Brand:'
+        $(row).find('td:eq(3)').attr 'data-title', 'Product:'
+        $(row).find('td:eq(4)').attr 'data-title', 'Serial:'
+        $(row).find('td:eq(5)').attr 'data-title', 'Power Status:'
+        $(row).find('td:eq(6)').attr 'data-title', 'Last Sync Status:'
+        $(row).find('td:eq(7)').attr 'data-title', 'Onboard Status:'
+        $(row).find('td:eq(8)').attr 'data-title', 'Synchronize Date:'
       buttons: [
         {
           extend: 'copyHtml5'
@@ -161,15 +163,15 @@ $(document).on 'turbolinks:load', ->
           selectRow: true
           }
         }
-        { targets: 9
+        { targets: 10
         orderable: false
         }
-        { targets: 7
+        { targets: 8
         orderable: false
         visible: false
         searchable: false
         }
-        { targets: 6
+        { targets: 7
         render: (data, type, full) ->
           if !data
             '<div class="blue-grey lighten-1 white-text z-depth-1 sync"><i class="fa fa-minus-circle" aria-hidden="true"></i> Not Onboarded</div>'
@@ -180,7 +182,7 @@ $(document).on 'turbolinks:load', ->
           else
             '<div class="red lighten-2 white-text z-depth-1 sync"><i class="fa fa-exclamation-triangle" aria-hidden="true"></i> ' + I18n.t(data, scope: 'filters.options.onboard_request.status') + ': ' + I18n.t(full.onboard_request_step, scope: 'filters.options.onboard_request.step') + '</div>'
         }
-        { targets: 4
+        { targets: 5
         render: (data, type, full, meta) ->
           if data == "on"
             '<div class="power_status green lighten-2 z-depth-1"><i class="fa fa-power-off"></i> On</div>'
@@ -190,7 +192,7 @@ $(document).on 'turbolinks:load', ->
             '<div class="black-text">N/A</div>'
         width: 50
         }
-        { targets: 5
+        { targets: 6
         render: (data, type, full) ->
           if !data
             '<div class="blue-grey darken-2 white-text z-depth-1 sync"><i class="fa fa-hourglass-start" aria-hidden="true"></i> Queued</div>'
@@ -205,7 +207,7 @@ $(document).on 'turbolinks:load', ->
         width: 200
         }
         { targets: 2
-        orderable: false
+        orderable: true
         render: (data, type, full, meta) ->
           if /(HP)/.test(data)
             '<div class="model_wrapper"><div class="img-box"><img src="/images/hpe.png" height=25 width=57 /></div><div class="model_cell">' + data + '</div></div>'
@@ -221,10 +223,10 @@ $(document).on 'turbolinks:load', ->
             '<div class="model_cell">N/A</div>'
           else
             '<div class="model_cell">' + data + '</div>'
-        width: 325
+        width: 175
         }
-        { targets: 3
-        orderable: false
+        { targets: 4
+        orderable: true
         render: (data, type, full, meta) ->
           if data
             '<div class="serial">' + data + '</div>'

app/assets/javascripts/zones.coffee
@@ -90,7 +90,8 @@ $(document).on 'turbolinks:load', ->
       columns: [
         {data: 'checkbox'}
         {data: 'ip_address'}
-        {data: 'system_model'}
+        {data: 'brand'}
+        {data: 'product'}
         {data: 'serial'}
         {data: 'power_status'}
         {data: 'sync_status'}
@@ -105,12 +106,13 @@ $(document).on 'turbolinks:load', ->
       select: 'multi'
       createdRow: (row, data, dataIndex) ->
         $(row).find('td:eq(1)').attr 'data-title', 'BMC Address:'
-        $(row).find('td:eq(2)').attr 'data-title', 'System Model:'
-        $(row).find('td:eq(3)').attr 'data-title', 'Serial:'
-        $(row).find('td:eq(4)').attr 'data-title', 'Power Status:'
-        $(row).find('td:eq(5)').attr 'data-title', 'Last Sync Status:'
-        $(row).find('td:eq(6)').attr 'data-title', 'Onboard Status:'
-        $(row).find('td:eq(7)').attr 'data-title', 'Synchronize Date:'
+        $(row).find('td:eq(2)').attr 'data-title', 'Brand:'
+        $(row).find('td:eq(3)').attr 'data-title', 'Product:'
+        $(row).find('td:eq(4)').attr 'data-title', 'Serial:'
+        $(row).find('td:eq(5)').attr 'data-title', 'Power Status:'
+        $(row).find('td:eq(6)').attr 'data-title', 'Last Sync Status:'
+        $(row).find('td:eq(7)').attr 'data-title', 'Onboard Status:'
+        $(row).find('td:eq(8)').attr 'data-title', 'Synchronize Date:'
       buttons: [
         {
           extend: 'copyHtml5'
@@ -131,15 +133,15 @@ $(document).on 'turbolinks:load', ->
           selectRow: true
           }
         }
-        { targets: 9
+        { targets: 10
         orderable: false
         }
-        { targets: 7
+        { targets: 8
         orderable: false
         visible: false
         searchable: false
         }
-        { targets: 6
+        { targets: 7
         render: (data, type, full) ->
           if !data
             '<div class="blue-grey lighten-1 white-text z-depth-1 sync"><i class="fa fa-minus-circle" aria-hidden="true"></i> Not Onboarded</div>'
@@ -150,7 +152,7 @@ $(document).on 'turbolinks:load', ->
           else
             '<div class="red lighten-2 white-text z-depth-1 sync"><i class="fa fa-exclamation-triangle" aria-hidden="true"></i> ' + I18n.t(data, scope: 'filters.options.onboard_request.status') + ': ' + I18n.t(full.onboard_request_step, scope: 'filters.options.onboard_request.step') + '</div>'
         }
-        { targets: 4
+        { targets: 5
         render: (data, type, full, meta) ->
           if data == "on"
             '<div class="power_status green lighten-2 z-depth-1"><i class="fa fa-power-off"></i> On</div>'
@@ -160,7 +162,7 @@ $(document).on 'turbolinks:load', ->
             '<div class="black-text">N/A</div>'
         width: 50
         }
-        { targets: 5
+        { targets: 6
         render: (data, type, full) ->
           if !data
             '<div class="blue-grey darken-2 white-text z-depth-1 sync"><i class="fa fa-hourglass-start" aria-hidden="true"></i> Queued</div>'
@@ -175,7 +177,7 @@ $(document).on 'turbolinks:load', ->
         width: 200
         }
         { targets: 2
-        orderable: false
+        orderable: true
         render: (data, type, full, meta) ->
           if /(HP)/.test(data)
             '<div class="model_wrapper"><div class="img-box"><img src="/images/hpe.png" height=25 width=57 /></div><div class="model_cell">' + data + '</div></div>'
@@ -191,10 +193,10 @@ $(document).on 'turbolinks:load', ->
             '<div class="model_cell">N/A</div>'
           else
             '<div class="model_cell">' + data + '</div>'
-        width: 325
+        width: 175
         }
-        { targets: 3
-        orderable: false
+        { targets: 4
+        orderable: true
         render: (data, type, full, meta) ->
           if data
             '<div class="serial">' + data + '</div>'

app/datatables/bmc_host_datatable.rb
@@ -6,7 +6,8 @@ class BmcHostDatatable < ApplicationDatatable
   def view_columns
     @view_columns ||= {
       ip_address: {source: "BmcHost.ip_address", orderable: true},
-      system_model: {source: "BmcHost.system_model"},
+      brand: {source: "BmcHost.brand"},
+      product: {source: "BmcHost.product"},
       serial: {source: "BmcHost.serial"},
       zone_name: {source: "Zone.name"},
       power_status: {source: "BmcHost.power_status", searchable: false, orderable: true},
@@ -20,7 +21,8 @@ class BmcHostDatatable < ApplicationDatatable
   def data
     records.map do |record| {
       ip_address: record.ip_address,
-      system_model: record.system_model,
+      brand: record.brand,
+      product: record.product,
       serial: record.serial,
       zone_name: record.zone.name,
       power_status: record.power_status,

app/datatables/bmc_scan_request_details_datatable.rb
@@ -6,7 +6,8 @@ class BmcScanRequestDetailsDatatable < ApplicationDatatable
   def view_columns
     @view_columns ||= {
       ip_address: {source: "BmcHost.ip_address"},
-      system_model: {source: "BmcHost.system_model"},
+      brand: {source: "BmcHost.brand"},
+      product: {source: "BmcHost.product"},
       serial: {source: "BmcHost.serial"},
       power_status: {source: "BmcHost.power_status", searchable: false, orderable: true},
       sync_status: {source: "BmcHost.sync_status", searchable: false, orderable: true},
@@ -19,7 +20,8 @@ class BmcScanRequestDetailsDatatable < ApplicationDatatable
   def data
     records.map do |record| {
       ip_address: record.ip_address,
-      system_model: record.system_model,
+      brand: record.brand,
+      product: record.product,
       serial: record.serial,
       power_status: record.power_status,
       sync_status: record.sync_status,

app/datatables/zone_details_datatable.rb
@@ -6,7 +6,8 @@ class ZoneDetailsDatatable < ApplicationDatatable
   def view_columns
     @view_columns ||= {
       ip_address: {source: "BmcHost.ip_address"},
-      system_model: {source: "BmcHost.system_model"},
+      brand: {source: "BmcHost.brand"},
+      product: {source: "BmcHost.product"},
       serial: {source: "BmcHost.serial"},
       power_status: {source: "BmcHost.power_status", searchable: false, orderable: true},
       sync_status: {source: "BmcHost.sync_status", searchable: false, orderable: true},
@@ -19,7 +20,8 @@ class ZoneDetailsDatatable < ApplicationDatatable
   def data
     records.map do |record| {
       ip_address: record.ip_address,
-      system_model: record.system_model,
+      brand: record.brand,
+      product: record.product,
       serial: record.serial,
       power_status: record.power_status,
       sync_status: record.sync_status,

app/views/bmc_hosts/index.html.erb
@@ -28,7 +28,8 @@
           <tr>
             <th></th>
             <th>BMC Address</th>
-            <th>System Model</th>
+            <th>Brand</th>
+            <th>Product</th>
             <th>Serial</th>
             <th>Datacenter Zone</th>
             <th>Power Status</th>

commit 1f66e69e4b2521490ed8ace87821f809289b597f

Fixed BmcHost regressions from 21e71d1
- MOD: Cleaned up debug messages and comments in ApiQuery
- MOD: Restored default timeout of SmartProxyApi to 30 seconds
- MOD: Wording fix in debug message in SmartProxyApiFactory
- FIX: Be much more patient (180 seconds) for slow BMC hosts when
       requesting FRU list
- FIX: Workaround for "NONE" garbage output from Supermicro FRU lists
- FIX: Corrected order of searches for BmcHost#product_from_fru_list

app/lib/dcim/api_query.rb
@@ -33,21 +33,15 @@ module Dcim
             kwargs[:headers] ||= {}
             kwargs[:headers].merge!({'Content-Type':'application/json'})
           rescue JSON::ParserError => e
-            puts payload
-            puts e
           end
         end
 
         options.merge!(kwargs)
 
-        #resource = RestClient::Resource.new(@resource.instance_variable_get(:@url),
-        #                                    **options)
-        #rest_query = resource[@query.join('/')]
         result = RestClient::Request.execute(options.merge(
               method: method,
               url: @resource.send(:concat_urls, url, @query.join('/')),
               payload: payload))
-        #result = rest_query.send(method, *payload)
         ApiResult.new(result)
       end
     end

app/lib/dcim/smart_proxy_api.rb
@@ -10,7 +10,7 @@ module Dcim
       privkey = Rails.configuration.smart_proxy['privkey']
       @resource = RestClient::Resource.new(kwargs[:url],
                                            proxy: "",
-                                           timeout: kwargs[:timeout] || 60,
+                                           timeout: kwargs[:timeout] || 30,
                                            open_timeout: 5,
                                            ssl_ca_file: ca_cert,
                                            ssl_client_cert: OpenSSL::X509::Certificate.new(File.read(cert)),

app/lib/dcim/smart_proxy_api_factory.rb
@@ -10,7 +10,7 @@ module Dcim
         smart_proxy_resource = @smart_proxies[location_id]
         raise Dcim::InvalidSmartProxyError unless smart_proxy_resource.is_a? Dcim::SmartProxyApi
         raise Dcim::InvalidSmartProxyError unless smart_proxy_resource.features.get.to_hash.include? "onboard"
-        logger.debug "Returning cached Smart Proxy for Foreman location ID #{location_id} because it is still viable"
+        logger.debug "Returning cached Smart Proxy for Foreman location ID #{location_id} because it is still suitable"
         return @smart_proxies[location_id]
       rescue RuntimeError
         logger.debug "No Smart Proxy currently cached for Foreman location ID #{location_id}"

app/models/bmc_host.rb
@@ -81,7 +81,7 @@ class BmcHost < ApplicationRecord
   end
 
   def fru_list
-    freeipmi_smart_proxy_bmc_request(smart_proxy.bmc(self.ip_address).fru.list)
+    freeipmi_smart_proxy_bmc_request(smart_proxy.bmc(self.ip_address).fru.list, timeout: 180)
   end
 
   def shutdown
@@ -121,21 +121,21 @@ class BmcHost < ApplicationRecord
     @foreman_resource ||= Dcim::ForemanApiFactory.instance
   end
 
-  def freeipmi_smart_proxy_bmc_request(query, payload: {}, method: :get)
-    http_smart_proxy_bmc_request(query, payload: {'bmc_provider':'freeipmi'}.merge(payload), method: method)
+  def freeipmi_smart_proxy_bmc_request(query, payload: {}, method: :get, **kwargs)
+    http_smart_proxy_bmc_request(query, payload: {'bmc_provider':'freeipmi'}.merge(payload), method: method, **kwargs)
   rescue Dcim::UnknownError
-    http_smart_proxy_bmc_request(query, payload: {'bmc_provider':'ipmitool'}.merge(payload), method: method)
+    http_smart_proxy_bmc_request(query, payload: {'bmc_provider':'ipmitool'}.merge(payload), method: method, **kwargs)
   end
 
-  def ipmitool_smart_proxy_bmc_request(query, payload: {}, method: :get)
-    http_smart_proxy_bmc_request(query, payload: {'bmc_provider':'ipmitool'}.merge(payload), method: method)
+  def ipmitool_smart_proxy_bmc_request(query, payload: {}, method: :get, **kwargs)
+    http_smart_proxy_bmc_request(query, payload: {'bmc_provider':'ipmitool'}.merge(payload), method: method, **kwargs)
   rescue Dcim::UnknownError
-    http_smart_proxy_bmc_request(query, payload: {'bmc_provider':'freeipmi'}.merge(payload), method: method)
+    http_smart_proxy_bmc_request(query, payload: {'bmc_provider':'freeipmi'}.merge(payload), method: method, **kwargs)
   end
 
-  def http_smart_proxy_bmc_request(query, payload: nil, method: :get)
+  def http_smart_proxy_bmc_request(query, payload: nil, method: :get, **kwargs)
     begin
-      result = query.send(method, user: self.username, password: self.password, payload: payload).to_hash["result"]
+      result = query.send(method, user: self.username, password: self.password, payload: payload, **kwargs).to_hash["result"]
     rescue RestClient::Unauthorized
       raise Dcim::InvalidCredentialsError
     end
@@ -176,7 +176,7 @@ class BmcHost < ApplicationRecord
   end
 
   def deep_find(key, object=self, found=nil)
-    if object.respond_to?(:key?) && object.key?(key)
+    if object.respond_to?(:key?) && object.key?(key) && object[key] != 'NONE'
       return object[key]
     elsif object.is_a? Enumerable
       object.find { |*a| found = deep_find(key, a.last) }
@@ -194,9 +194,9 @@ class BmcHost < ApplicationRecord
 
   def product_from_fru_list(fru)
     output =
-    deep_find('product_part/model_number', fru) ||
     deep_find('product_name', fru) ||
-    deep_find('board_product_name', fru)
+    deep_find('board_product_name', fru) ||
+    deep_find('product_part/model_number', fru)
     raise Dcim::UnsupportedFruError, fru if !output
     output
   end