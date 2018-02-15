98bc5ceeecb66f2cd2e0059743109bcdfaff1649
Nick Liu <deltik@gmx.com>
2018-02-15 10:23:45 -0600

    Reworked routes
    - MOD: Routes now have shorter names. Should be less of an eyesore.

--- config/routes.rb
+++ config/routes.rb
@@ -3,27 +3,11 @@ Sidekiq::Web.set :session_secret, Rails.application.secrets[:secret_key_base]
 
 Rails.application.routes.draw do
   get 'admin', to: 'admin#index'
+  # /admin
   namespace :admin do
-    resources :brute_lists, path: 'bmc_scan_requests/bmc_credentials', except: %i[edit new] do
-      collection do
-        get :datatable, to: 'brute_lists#datatable', klass: Admin::BruteListDatatable
-      end
-    end
-    resources :onboard_requests, path: 'bmc_hosts/onboard_requests', except: %i[edit new] do
-      collection do
-        get :datatable, to: 'onboard_requests#datatable', klass: Admin::OnboardRequestDatatable
-        post :new_modal
-      end
-      member do
-        get :bmc_hosts_datatable, to: 'onboard_requests#datatable', klass: Admin::OnboardRequestDetailsDatatable
-      end
-    end
-    resources :enclosure_racks, path: 'datacenter_zones/enclosure_racks', except: %i[edit new] do
-      collection do
-        get :datatable, to: 'enclosure_racks#datatable', klass: Admin::EnclosureRackDatatable
-      end
-    end
-    resources :zones, path: 'datacenter_zones', except: %i[edit new] do
+    # ./zones
+    # ./zone/{id}
+    resources :zones, path: 'zones', except: %i[edit new] do
       # resources :enclosure_racks, shallow: true, path: 'racks'
       collection do
         post :foreman_remove
@@ -36,15 +20,24 @@ Rails.application.routes.draw do
         get :bmc_hosts_datatable, to: 'zones#datatable', klass: Admin::ZoneDetailsDatatable
       end
     end
-    get :check_foreman_locations_synced, controller: :zones
-    resources :bmc_hosts, path: 'bmc_hosts' do
+    # ./racks/{id}
+    resources :enclosure_racks, path: 'racks', except: %i[edit new] do
       collection do
-        post :multi_action
-        post :new_modal
-        get :datatable, to: 'bmc_hosts#datatable', klass: Admin::BmcHostDatatable
+        get :datatable, to: 'enclosure_racks#datatable', klass: Admin::EnclosureRackDatatable
       end
     end
-    resources :bmc_scan_requests, path: 'bmc_scan_requests', except: %i[edit new] do
+    # ./enclosure/{id}
+    # ./device/{id}
+    # ./bmc_hosts/credentials
+    # ./bmc_hosts/credential/{id}
+    resources :brute_lists, path: 'bmc_hosts/credentials', except: %i[edit new] do
+      collection do
+        get :datatable, to: 'brute_lists#datatable', klass: Admin::BruteListDatatable
+      end
+    end
+    # ./bmc_hosts/scans
+    # ./bmc_hosts/scan/{id}
+    resources :bmc_scan_requests, path: 'bmc_hosts/scans', except: %i[edit new] do
       collection do
         get :datatable, to: 'bmc_scan_requests#datatable', klass: Admin::BmcScanRequestDatatable
       end
@@ -52,6 +45,33 @@ Rails.application.routes.draw do
         get :bmc_hosts_datatable, to: 'bmc_scan_requests#datatable', klass: Admin::BmcScanRequestDetailsDatatable
       end
     end
+    # ./bmc_hosts/onboards
+    # ./bmc_hosts/onboard/{id}
+    resources :onboard_requests, path: 'bmc_hosts/onboards', except: %i[edit new] do
+      collection do
+        get :datatable, to: 'onboard_requests#datatable', klass: Admin::OnboardRequestDatatable
+        post :new_modal
+      end
+      member do
+        get :bmc_hosts_datatable, to: 'onboard_requests#datatable', klass: Admin::OnboardRequestDetailsDatatable
+      end
+    end
+    # ./bmc_hosts
+    # ./bmc_host/{id}
+    resources :bmc_hosts, path: 'bmc_hosts' do
+      collection do
+        post :multi_action
+        post :new_modal
+        get :datatable, to: 'bmc_hosts#datatable', klass: Admin::BmcHostDatatable
+      end
+    end
+    # ./systems
+    # ./system/{id}
+    # ./visualdc/zone/{id}
+    # ./visualdc/rack/{id}
+    # ./visualdc/enclosure/{id}
+
+    get :check_foreman_locations_synced, controller: :zones
     get :check_foreman_reachable, controller: :bmc_scan_requests
   end
   resources :systems
