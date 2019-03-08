7c1af4f26624c48fb77b412741ce384bc4041394
Deltik <deltik@gmx.com>
2019-03-08 15:18:08 -0600

    Added GraphQL mutation for changing Component label and managed mode

---
+++ app/graphql/mutations/update_component.rb
@@ -0,0 +1,25 @@
+module Mutations
+  class UpdateComponent < BaseMutation
+    argument :id, ID, required: true
+    argument :label, String, required: false
+    argument :managed, Boolean, required: false
+
+    field :component, Types::ComponentType, null: true
+    field :errors, [String], null: false
+
+    def resolve(**kwargs)
+      component = Component.find(kwargs[:id])
+      if component.update(**kwargs)
+        {
+            component: component,
+            errors: []
+        }
+      else
+        {
+            component: nil,
+            errors: component.errors.full_messages
+        }
+      end
+    end
+  end
+end
